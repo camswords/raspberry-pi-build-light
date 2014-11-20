1d6b96a8c8eebc5167e77208dd3add9f64d44f5a
Cam Swords <camswords@Cams-MacBook-Pro.local>
2014-11-21 00:05:27 +1100

    spiked the ability to turn lights on and off remotely

--- src/support-team.js
+++ src/support-team.js
@@ -1,19 +1,20 @@
 var projectsRepository = require('./projects-repository');
+var lights = require('./lights');
 
 module.exports = {
     notify: function() {
         var projects = projectsRepository.retrieve();
 
         if (projects.hasBuildingBuild()) {
-            console.log('building');
+            lights.turnOnLightOne();
         }
 
         if (projects.hasSuccessfulBuild()) {
-            console.log('success');
+            lights.turnOnLightTwo();
         }
 
         if (projects.hasFailedBuild()) {
-            console.log('failure');
+            lights.turnOnLightThree();
         }
     }
 };
