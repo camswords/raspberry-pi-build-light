27629dce0b9e2af02fdaceb55eb8e225fd05381c
Cam Swords <camswords@Cams-MacBook-Pro.local>
2014-11-26 11:46:39 +1100

    wait a bit of time in between turning lights on and off

--- power-on.js
+++ power-on.js
@@ -2,6 +2,7 @@
 var snapCI = require('./src/snap-ci');
 var supportTeam = require('./src/support-team');
 
+snapCI.poll();
 setInterval(snapCI.poll, 10000);
-setInterval(supportTeam.notify, 1000);
+setInterval(supportTeam.notify, 5000);
 
--- src/support-team.js
+++ src/support-team.js
@@ -5,19 +5,31 @@ module.exports = {
     notify: function() {
         var projects = projectsRepository.retrieve();
 
-        if (projects.hasBuildingBuild()) {
-            console.log('building');
-            lights.turnOnLightOne();
-        }
 
-        if (projects.hasSuccessfulBuild()) {
-            console.log('success');
-            lights.turnOnLightTwo();
-        }
 
-        if (projects.hasFailedBuild()) {
-            console.log('failure');
-            lights.turnOnLightThree();
-        }
+        var building = function() {
+            if (projects.hasBuildingBuild()) {
+                console.log('building');
+                lights.turnOnLightOne();
+            }
+        };
+
+        var successful = function() {
+            if (projects.hasSuccessfulBuild()) {
+                console.log('success');
+                lights.turnOnLightTwo();
+            }
+        };
+
+        var failed = function() {
+            if (projects.hasFailedBuild()) {
+                console.log('failure');
+                lights.turnOnLightThree();
+            }
+        };
+
+        building();
+        setTimeout(successful, 1000);
+        setTimeout(failed, 2000);
     }
 };
