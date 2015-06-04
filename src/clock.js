fd6059e43cccc5d17ee54d83380ccf8946060a3c
Cam Swords <cam.swords@gmail.com>
2015-06-04 11:51:24 +1000

    fixed the error logging with clock.now

--- src/clock.js
+++ src/clock.js
@@ -1,6 +1,9 @@
 var moment = require('moment-timezone');
 
 module.exports = {
+    now: function() {
+        return moment().tz('Australia/Sydney').format();
+    },
     bedTime: function() {
         var hourOfDay = parseInt(moment().tz('Australia/Sydney').format('H'));
         return hourOfDay < 9 || hourOfDay > 18;
