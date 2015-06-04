e1fd111dd252b933c8831038dbf33121efc7bbda
Cam Swords <cam.swords@gmail.com>
2015-06-04 12:16:34 +1000

    update the ip every hour

--- power-on.js
+++ power-on.js
@@ -6,5 +6,5 @@ var diagnostics = require('./src/diagnostics');
 snapCI.poll();
 setInterval(snapCI.poll, 10000);
 setInterval(supportTeam.notify, 5000);
-setInterval(diagnostics.run, 60000);
+setInterval(diagnostics.run, 3600000);
 
