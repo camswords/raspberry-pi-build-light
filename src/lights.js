83a6baa5eee55dc7afbcc8b7dc6b4f50b4dcdef4
Cam Swords <cam.swords@gmail.com>
2015-06-10 09:16:41 +1000

    turn all the lights off, not just light two

--- src/lights.js
+++ src/lights.js
@@ -17,7 +17,11 @@ module.exports = {
     turnOnLightThree: function() {  execy('/usr/bin/send 001000001111101000001010 0 2'); },
     turnOffLightThree: function() { execy('/usr/bin/send 001000001111101000000010 0 2'); },
     turnAllLightsOn: function() {   execy('/usr/bin/send 001000001111101000001001 0 2'); },
-    turnAllLightsOff: function() { execy('/usr/bin/send 001000001111101000000100 0 2'); }
+    turnAllLightsOff: function() {
+        execy('/usr/bin/send 001000001111101000000110 0 2');
+        execy('/usr/bin/send 001000001111101000000100 0 2');
+        execy('/usr/bin/send 001000001111101000000010 0 2');
+    }
 };
 
 
