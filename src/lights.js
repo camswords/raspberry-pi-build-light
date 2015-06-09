a5eda0385f30c70f5c90e54d2522d96d12b017ad
Cam Swords <cam.swords@gmail.com>
2015-06-10 09:01:13 +1000

    change the lights to just turn off light two, to see why its not working

--- src/lights.js
+++ src/lights.js
@@ -8,11 +8,7 @@ module.exports = {
     turnOnLightThree: function() {  exec('/usr/bin/send 001000001111101000001010 0 2'); },
     turnOffLightThree: function() { exec('/usr/bin/send 001000001111101000000010 0 2'); },
     turnAllLightsOn: function() {   exec('/usr/bin/send 001000001111101000001001 0 2'); },
-    turnAllLightsOff: function() {
-        exec('/usr/bin/send 001000001111101000000110 0 2');
-        exec('/usr/bin/send 001000001111101000000100 0 2');
-        exec('/usr/bin/send 001000001111101000000010 0 2');
-    }
+    turnAllLightsOff: function() { exec('/usr/bin/send 001000001111101000000100 0 2'); }
 };
 
 
